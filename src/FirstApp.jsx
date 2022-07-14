b06cfbf0111611c286086c4aa972f46041f8d43c
Marco Garcia <dev.84ui7@aleeas.com>
2022-07-14 16:39:17 -0500

    Using function into jsx with js expression

---
+++ src/FirstApp.jsx
@@ -0,0 +1,11 @@
+const sayHello = () => 'Hello you';
+
+export const FirstApp = () => {
+
+    return (
+        <>
+            <h1> {sayHello()} </h1>
+            <p>Soy un sub</p>
+        </>
+    )
+}

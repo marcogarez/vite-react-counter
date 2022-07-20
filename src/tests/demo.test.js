9c425835b0459e9914caccc6768390a6a60076f6
marcogarez <dev.84ui7@aleeas.com>
2022-07-20 16:30:20 -0500

    Using describe, test, expect and toBe on jest test

---
+++ src/tests/demo.test.js
@@ -0,0 +1,11 @@
+describe('Pruebas en <Componente />', () => {
+    test('Esta prueba no debe fallar', () => {
+
+        const message1 = 'Hola mundo';
+        
+        const message2 = message1.trim();
+
+        expect( message1 ).toBe( message2 );
+
+    })
+});
